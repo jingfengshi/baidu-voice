fa4009f959d653e730536366c701f56d759560c5
jingfengshi <240023810@qq.com>
2019-01-04 09:12:27 +0000

    Apply fixes from StyleCI

--- tests/AiSpeechTest.php
+++ tests/AiSpeechTest.php
@@ -6,22 +6,18 @@ use jingfengshi\BaiduVoice\AipSpeech;
 use jingfengshi\BaiduVoice\Exceptions\InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
-
 class AiSpeechTest extends TestCase
 {
-
     public function testGetContentFromVoiceInvalidType()
     {
-        $w = new AipSpeech('mock-key','mock-key','mock');
+        $w = new AipSpeech('mock-key', 'mock-key', 'mock');
 
         $this->expectException(InvalidArgumentException::class);
 
         $this->expectExceptionMessage('语音类型非法: mp3');
 
-        $w->getContentFromVoice('test','mp3');
+        $w->getContentFromVoice('test', 'mp3');
 
         $this->fail('Failed to assert getWeather throw exception with invalid argument.');
-
     }
-
 }
